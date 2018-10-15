111b7b86a78a03118bbc7b680d19e4afb5b61c09
R-N84 <30769605+R84IEHMTS00x@users.noreply.github.com>
2018-10-16 01:35:07 +0900

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,3 @@
-//import React, { Component } from 'react';
-
-//class App extends Component {
-//  render() {
-//    return <h1>Hello, World!</h1>;
-//  }
-//}
-
-//class App extends Component {
-//  render() {
-//    const dom = <h1>Hi!</h1>
-//    return dom;
-//  }
-//}
-
-//class App extends Component {
-//  render() {
-//    const greeting = "Ethereum";
-//    const dom = <h1 className="foo">{greeting}</h1>;
-//    return dom;
-//  }
-//}
-
-//class App extends Component {
-//  render() {
-//    return (
-//      <React.Fragment>
-//        <label htmlFor="bar">bar</label>
-//        <input type="text" onChange={() => {console.log("I am clicked!")}} />
-//      </React.Fragment>
-//    )
-//  }
-//}
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
@@ -45,18 +12,13 @@ class App extends Component {
       <React.Fragment>
         <div class="count">Count App {props.value}</div>
         <button class="plus" onClick={props.increment}>Plus</button>
-        <button class="Minus" onClick={props.decrement}>Minus</button>
+        <button class="minus" onClick={props.decrement}>Minus</button>
       </React.Fragment>
     )
   }
 }
 
 const mapStateToProps = state => ({ value: state.count.value })
-
-// const mapDispatchToProps = dispatch => ({
-//   increment: () => dispatch(increment()),
-//   decrement: () => dispatch(decrement())
-// })
 
 const mapDispatchToProps = ({ increment, decrement })
 
